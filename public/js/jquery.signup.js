272b57cbcdd38c326bdd905952f70dee14d8b9cb
Raul Marcos <raulmarcosl@gmail.com>
2013-03-19 03:52:42 +0100

    Libraries needed

---
+++ public/js/jquery.signup.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    $('#signup_submit').click(function () {
+        var hashPass = CryptoJS.MD5($('#password').val()).toString();
+
+        $('#password').val(hashPass);
+
+        $('signup_submit').submit();
+    });
+
+});
