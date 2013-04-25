46260c9919ca8071783aa6031df467454dc82a29
Raul Marcos <raulmarcosl@gmail.com>
2013-04-25 18:44:25 +0200

    Changing port

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ db.on('error', console.error.bind(console, "Connection error: "));
 var app = express();
 
 app.configure(function(){
-    app.set('port', process.env.PORT || 5000);
+    app.set('port', process.env.PORT || 3000);
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
     app.use(express.favicon());
