9b5f64de43fee0ba2e78c1061c8b66a66eb48177
antonioIrizar <antonioirizar@gmail.com>
2013-05-19 16:47:28 +0200

    add functions crypto qr

--- routes/apiEvent.js
+++ routes/apiEvent.js
@@ -10,10 +10,14 @@ db.on('error', console.error.bind(console, "Connection error: "));
 
 var allSchemas = require('../models/allSchemas'),
     // qrCode = require('qrcode-npm/qrcode'),
+    crypto = require('crypto'),
     fs = require('fs');
 
 var Event = mongoose.model('Event');
 var User = mongoose.model('User');
+
+//clave para los qr
+var superKey = 'super_key';
 
 exports.save = function (req, res) {
     var userId, userPassword;
@@ -252,3 +256,25 @@ exports.dontGoToEvent = function (req, res) {
     });
 };
 
+function encrypt (key,plaintext){
+
+    var cipher = crypto.createCipher('aes-256-cbc', key),encryptedPassword;
+
+    cipher.update(plaintext, 'utf8', 'base64');
+    encryptedPassword = cipher.final('base64');
+    console.log('encrypted :', encryptedPassword);
+
+    return encryptedPassword;
+
+};
+
+function decrypt (key,encryptedPassword){
+
+    var decipher = crypto.createDecipher('aes-256-cbc', key),decryptedPassword;
+
+    decipher.update(encryptedPassword, 'base64', 'utf8'); 
+    decryptedPassword = decipher.final('utf8');
+    console.log('decrypted :', decryptedPassword);
+
+    return decryptedPassword;
+};
